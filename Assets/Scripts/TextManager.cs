ee9b61f7115f87d14f00ffa469b25e293aa0e4f5
ISpyDeveloperPie <112821562+ISpyDeveloperPie@users.noreply.github.com>
2024-06-14 17:31:28 -0700

    Fixed skip bug and added sounds
    Dialogue max speed is 0.95

--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -13,17 +13,20 @@ using TMPro;
 using UnityEditor;
 using UnityEngine.SceneManagement;
 using static DecisionList;
-using UnityEngine.Networking;
 //using UnityEngine.UI;
 
 public class TextManager : MonoBehaviour
 {
-    public float text_delay = 0.06f;
+    public float text_delay = 0.1f;
     public String speaker_name;
+<<<<<<< Updated upstream
+    public String DEBUG_TEXT = "[0.1]This is a message for debugging!";
+=======
     public String DEBUG_TEXT = "[0.3]This is a message for debugging!";
     public List<AudioClip> text_audio_clips;
     private AudioSource audioSource;
     private int last_played_index = -1;
+>>>>>>> Stashed changes
     private Label txt;
     private VisualElement txt_container;
     private Label name_txt;
@@ -34,13 +37,9 @@ public class TextManager : MonoBehaviour
     private VisualElement decision_button_container;
     private Button decision_button_1;
     private Button decision_button_2;
-    private Button decision_button_3;
-    private VisualElement decision_3_background;
-    private VisualElement character_image;
 
     private Action b1_delegate;
     private Action b2_delegate;
-    private Action b3_delegate;
 
     private DecisionManager decision_manager;
 
@@ -48,14 +47,6 @@ public class TextManager : MonoBehaviour
     private List<string> message_que = new List<string>();
     [SerializeField]
     private List<(DecisionList, int)> decision_que = new List<(DecisionList, int)>();
-    [SerializeField]
-    private List<(string, int)> speaker_que = new List<(string, int)>();
-    [SerializeField]
-    private List<(CustomFunction, int)> function_que = new List<(CustomFunction, int)>();
-    [SerializeField]
-    private List<(float, int)> pause_que = new List<(float, int)>();
-    [SerializeField]
-    private List<(string, int)> image_que = new List<(string, int)>();
 
     private void OnEnable()
     {
@@ -67,10 +58,10 @@ public class TextManager : MonoBehaviour
         decision_button_container = root.Q<VisualElement>("DecisionContainer");
         decision_button_1 = root.Q<Button>("Decision1Button");
         decision_button_2 = root.Q<Button>("Decision2Button");
-        decision_button_3 = root.Q<Button>("Decision3Button");
-        decision_3_background = root.Q<VisualElement>("Decision3Background");
+<<<<<<< Updated upstream
+=======
         audioSource = GetComponent<AudioSource>();
-        character_image = root.Q<VisualElement>("CharacterImage");
+>>>>>>> Stashed changes
 
         GameObject[] rootObjects = SceneManager.GetActiveScene().GetRootGameObjects();
         foreach (GameObject obj in rootObjects)
@@ -90,67 +81,48 @@ public class TextManager : MonoBehaviour
             //skip_txt = true;
         }
     }
-    public void AddCharacterChange(string image_path)
-    {
-        image_que.Add((image_path, message_que.Count()));
-    }
-    public void AddSpeakerChange(String speaker)
-    {
-        //speaker_name = speaker;
-        speaker_que.Add((speaker, message_que.Count()));
-        if (speaker_que.Count == 0)
-        {
-            ChangeSpeaker(speaker);
-        }
-    }
-
-    private void ChangeSpeaker(String speaker)
+    public void SetSpeaker(String speaker)
     {
         speaker_name = speaker;
-        name_txt.text = speaker_name;
-        //ChangeOutlineColor(col);
-    }
-
-    public void AddCustomFunction(CustomFunction customFunction)
-    {
-        function_que.Add((customFunction, message_que.Count()));
-    }
-
-    public void AddPause(float pause)
-    {
-        pause_que.Add((pause, message_que.Count()));
-    }
-
-    private void ChangeOutlineColor(Color col)
-    {
-        /*txt_container.style.borderBottomColor = col;
-        txt_container.style.borderLeftColor = col;
-        txt_container.style.borderRightColor = col;
-        txt_container.style.borderTopColor = col;
-        nametxt_container.style.backgroundColor = col;*/
     }
 
     [ContextMenu("DebugMessageQue")]
     private void DebugMessageQue()
     {
+        AddMessage(DEBUG_TEXT);
+<<<<<<< Updated upstream
+        AddMessage("[0.03]This is an internal message that is second in the list.");
+        AddMessage("[0.01]cool");
+=======
+        AddMessage("[0.095]This is an internal message that is second in the list.");
+        AddMessage("[0.095]cool");
+>>>>>>> Stashed changes
+
         DecisionList new_dl = new DecisionList();
         new_dl.text_1 = "Option1";
         new_dl.text_2 = "Option2";
         new_dl.decision1 = DebugOption1;
         new_dl.decision2 = DebugOption1;
-        AddSpeakerChange("Speaker2");
-        AddMessageWithDecision("[0.06]Choose an option", new_dl);
+<<<<<<< Updated upstream
+        AddMessageWithDecision("[0.1]Choose an option", new_dl);
+=======
+        AddMessageWithDecision("[0.092]Choose an option", new_dl);
+>>>>>>> Stashed changes
 
         DecisionList new_dl_2 = new DecisionList();
         new_dl_2.text_1 = "Option3";
         new_dl_2.text_2 = "Option4";
-        new_dl_2.text_3 = "Option5";
         new_dl_2.decision1 = DebugOption1_2;
         new_dl_2.decision2 = DebugOption1_2;
-        new_dl_2.decision3 = DebugOption1_2;
-        AddMessageWithDecision("[0.06]Choose another option", new_dl_2);
+<<<<<<< Updated upstream
+        AddMessageWithDecision("[0.1]Choose another option", new_dl_2);
 
-        AddMessage("[0.06]thanks!");
+        AddMessage("[0.01]thanks!");
+=======
+        AddMessageWithDecision("[0.095]Choose another option", new_dl_2);
+
+        AddMessage("[0.095]thanks!");
+>>>>>>> Stashed changes
 
         PlayMessageQue();
     }
@@ -178,8 +150,11 @@ public class TextManager : MonoBehaviour
     {
         nametxt_container.visible = true;
         txt_container.visible = true;
-        character_image.visible = true;
-        //name_txt.text = speaker_name;
+        name_txt.text = speaker_name;
+<<<<<<< Updated upstream
+        print("FIRST");
+=======
+>>>>>>> Stashed changes
         StartCoroutine("AnimateText");
     }
 
@@ -187,59 +162,27 @@ public class TextManager : MonoBehaviour
     {
         nametxt_container.visible = false;
         txt_container.visible = false;
-        character_image.visible = false;
         name_txt.text = "";
-        //ChangeOutlineColor(new Color(0, 1, 0));
-        if (function_que.Count() > 0)
-        {
-            if (function_que[function_que.Count() - 1].Item2 == message_que.Count())
-            {
-                (CustomFunction, int) function_info = function_que[function_que.Count() - 1];
-                function_info.Item1.custom_function();
-            }
-        }
         message_que.Clear();
-        decision_que.Clear();
-        function_que.Clear();
-        pause_que.Clear();
     }
 
     IEnumerator AnimateText()
     {
         float current_text_delay = text_delay;
+<<<<<<< Updated upstream
+        print("BBBBBBBB");
+        if (txt != null)
+        {
+            print("EEEEEE");
+            for (int k = 0; k < message_que.Count; k++)
+            {
+                print("AAAAAAAA");
+=======
         if (txt != null)
         {
             for (int k = 0; k < message_que.Count; k++)
             {
-                if (speaker_que.Any(t => t.Item2 == k))
-                {
-                    (string, int) speaker_info = speaker_que.Find(t => t.Item2 == k);
-                    ChangeSpeaker(speaker_info.Item1);
-                }
-
-                if (image_que.Any(t => t.Item2 == k))
-                {
-                    (string, int) image_info = image_que.Find(t => t.Item2 == k);
-                    StartCoroutine(SetCharacterImage(image_info.Item1));
-                }
-
-                if (function_que.Any(t => t.Item2 == k))
-                {
-                    (CustomFunction, int) function_info = function_que.Find(t => t.Item2 == k);
-                    function_info.Item1.custom_function();
-                }
-
-                if (pause_que.Any(t => t.Item2 == k))
-                {
-                    (float, int) pause_info = pause_que.Find(t => t.Item2 == k);
-                    nametxt_container.visible = false;
-                    txt_container.visible = false;
-                    txt.text = "";
-                    yield return new WaitForSeconds(pause_info.Item1);
-                    nametxt_container.visible = true;
-                    txt_container.visible = true;
-                }
-
+>>>>>>> Stashed changes
                 current_txt_end = false;
                 var text = message_que[k];
                 //print("ALIVE4");
@@ -285,7 +228,10 @@ public class TextManager : MonoBehaviour
                         delay_times.RemoveAt(0);
                     }
                     yield return new WaitForSeconds(current_text_delay);
+<<<<<<< Updated upstream
+=======
                     PlayRandomClip();
+>>>>>>> Stashed changes
                     txt.text = text.Substring(0, i);
                 }
                 skip_txt = false;
@@ -298,32 +244,15 @@ public class TextManager : MonoBehaviour
                         decision_button_2.clicked -= b2_delegate;
                     }
                     DecisionList dl = decision_que.Find(t => t.Item2 == k).Item1;
-                    decision_3_background.visible = false;
                     decision_button_container.visible = true;
-                    decision_3_background.visible = false;
-                    decision_button_1.visible = true;
-                    decision_button_2.visible = true;
                     decision_button_1.text = dl.text_1; 
                     decision_button_2.text = dl.text_2;
                     b1_delegate = () => OnOptionClick(dl, 1);
                     b2_delegate = () => OnOptionClick(dl, 1);
                     decision_button_1.clicked += b1_delegate;
                     decision_button_2.clicked += b2_delegate;
-
-                    if (dl.decision3 != null)
-                    {
-                        print("THERE IS A THIRD");
-                        if (b3_delegate != null)
-                        {
-                            decision_button_3.clicked -= b3_delegate;
-                        }
-                        decision_button_3.clicked -= b3_delegate;
-                        decision_button_3.visible = true;
-                        decision_3_background.visible = true;
-                        decision_button_3.text = dl.text_3;
-                        b3_delegate = () => OnOptionClick(dl, 1);
-                        decision_button_3.clicked += b3_delegate;
-                    }
+                    decision_button_1.visible = true;
+                    decision_button_2.visible = true;
                 }
                 yield return WaitForMouse();
             }
@@ -338,7 +267,11 @@ public class TextManager : MonoBehaviour
         {
             while (question_hold)
             {
+<<<<<<< Updated upstream
+                if (Input.GetMouseButton(0))
+=======
                 if (Input.GetMouseButtonDown(0))
+>>>>>>> Stashed changes
                 {
                     done = true;
                 }
@@ -349,7 +282,11 @@ public class TextManager : MonoBehaviour
         {
             while (!done)
             {
+<<<<<<< Updated upstream
+                if (Input.GetMouseButton(0))
+=======
                 if (Input.GetMouseButtonDown(0))
+>>>>>>> Stashed changes
                 {
                     done = true;
                 }
@@ -358,49 +295,46 @@ public class TextManager : MonoBehaviour
         }
     }
 
-    private IEnumerator SetCharacterImage(string filePath)
-    {
-        Texture2D tex = Resources.Load<Texture2D>(filePath);
-        //character_image.style.backgroundImage = tex;
-        character_image.style.backgroundImage = tex;
-        yield return null;
-    }
-
     void OnOptionClick(DecisionList chosen_decision_list, int decision_index)
     {
         decision_button_container.visible = false;
         decision_button_1.visible = false;
         decision_button_2.visible = false;
-        decision_button_3.visible = false;
-        decision_3_background.visible = false;
         print("Called Click!");
         if (decision_index == 1)
         {
             chosen_decision_list.decision1();
         }
-        else if (decision_index == 2)
+        else
         {
             chosen_decision_list.decision2();
-        }
-        else if (decision_index == 3)
-        {
-            chosen_decision_list.decision3();
         }
         question_hold = false;
     }
 
+<<<<<<< Updated upstream
+=======
     public void PlayRandomClip()
     {
-        /*int newIndex;
+        int newIndex;
         do
         {
             newIndex = UnityEngine.Random.Range(0, text_audio_clips.Count);
         } while (newIndex == last_played_index);
 
-        last_played_index = newIndex;*/
+        last_played_index = newIndex;
 
-        //AudioClip clip = (AudioClip)text_audio_clips[0];
-        //audioSource.clip = clip;
+        AudioClip clip = (AudioClip)text_audio_clips[newIndex];
+        audioSource.clip = clip;
         audioSource.Play();
+    }
+
+>>>>>>> Stashed changes
+    public void StopMessage()
+    {
+        nametxt_container.visible = false;
+        txt_container.visible = false;
+        name_txt.text = "";
+        txt.text = "";
     }
 }
