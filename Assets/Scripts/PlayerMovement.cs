b0f0b37389ebcf37b9c5a446d681ce70910219e2
JoshWilger <hap3guy@gmail.com>
2024-06-20 23:14:21 -0500

    Player health decreases when hitting "Obstacle" labelled objects

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float movementMultiplier = 5f;
     Rigidbody playerRb;
 
     // Start is called before the first frame update
@@ -18,6 +19,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float zInput = Input.GetAxisRaw("Vertical");
 
+        playerRb.AddForce(new Vector3(xInput, 0, zInput) * movementMultiplier);
     }
 }
