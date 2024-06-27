172aa49d130ff50c252e578e675d89b3d937e2aa
JoshWilger <hap3guy@gmail.com>
2024-06-26 23:15:43 -0500

    Edited a few things and added music and sound

--- Assets/Scripts/StartScreenButton.cs
+++ Assets/Scripts/StartScreenButton.cs
@@ -5,6 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class StartScreenButton : MonoBehaviour
 {
+	public AudioSource buttonSource;
+	public AudioClip hoverSound;
+	public AudioClip clickSound;
+
+	public void HoverSound() {
+		buttonSource.PlayOneShot(hoverSound);
+	}
+
+	public void ClickSound()
+	{
+		buttonSource.PlayOneShot(clickSound);
+	}
+
 	public void LoadGame(){
 		//2 corresponds to VideoScene
 		SceneManager.LoadScene(2);
